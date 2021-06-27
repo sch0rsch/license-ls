53348078af3bdcfd504d7ef70e83696d32cbec0c
sch0rsch <schorsch@wg-e26.de>
2021-06-27 15:56:47 +0200

    fix error handling in toCSV(...)

--- formatters/csv.js
+++ formatters/csv.js
@@ -9,7 +9,7 @@ const { parseAsync } = require('json2csv')
  */
 module.exports = async function (data, delimiter = ',') {
     try {
-        return parseAsync(data, { delimiter })
+        return await parseAsync(data, { delimiter })
     } catch(err) {
         console.warn('There was an error generating the CSV output', err)
         return ''
--- tests/csv.test.js
+++ tests/csv.test.js
@@ -1,7 +1,38 @@
 const toCsv = require('../formatters/csv')
 const test = require('ava')
 
-test('Does the thing', async (t) => {
-    // TODO: how do I test this since it's not a function?
-    t.true(true)
+test('Generates CSV with default comma delimiter', async (t) => {
+    const input = [
+        { name: 'Package A', version: '5.2.1' },
+        { name: 'Package B', version: '0.2'}
+    ]
+    const actual = await toCsv(input)
+    const [header, packageA, packageB, ...rest] = actual.split(/\r?\n/)
+
+    t.is(header, '"name","version"')
+    t.is(packageA, '"Package A","5.2.1"')
+    t.is(packageB, '"Package B","0.2"')
+    t.deepEqual(rest, [])
+})
+
+test('Generates CSV with passed in delimiter', async (t) => {
+    const input = [
+        { name: 'Package A', version: '5.2.1' },
+    ]
+    const actual = await toCsv(input, ';')
+    const [header, ...rest] = actual.split(/\r?\n/)
+
+    t.is(header, '"name";"version"')
+    t.is(rest.length, 1)
+})
+
+test('Returns empty output on error', async(t) => {
+    const invalidInput = {
+        prev: 'Previous',
+        next: 'Next'
+    }
+    invalidInput.next = invalidInput;
+    const actual = await toCsv(invalidInput);
+
+    t.is(actual, '')
 })
